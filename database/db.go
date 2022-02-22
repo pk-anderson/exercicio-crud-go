990eda9276af74d163e08142f57d7e8ddd7cc349
Patrick Anderson <71664004+pk-anderson@users.noreply.github.com>
2022-02-22 12:36:00 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func StartDatabase() *sql.DB {
-	db, err := sql.Open("mysql", "root:hbkpatrick@tcp(localhost:3306)/crud_usuario")
+	db, err := sql.Open("mysql", "root:{*}@tcp(localhost:3306)/crud_usuario")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
